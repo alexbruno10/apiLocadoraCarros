0f1ee05d2081eff6ffe358870e2df9786e62719b
unknown <alex_brunoneri@hotmail.com>
2021-09-28 20:27:06 -0300

    Criado os métodos Fillable e Rules para o Model Modelo

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['marca_id', 'imagem', 'nome', 'numero_portas', 'lugares', 'air_bag', 'abs'];
+
+    public function rules(){
+        return [
+            'marca_id' => 'exists:marcas,id',
+            'nome' => 'required|unique:marcas,nome,'.$this->id.'|min:3',
+            'imagem' => 'required|file|mimes:png, jpg, jpeg'
+        ];
+
+        /*
+            1) tabela
+            2) nome da coluna que será pesquisada na tabela3
+            3) id do registro que será desconsiderado na pesquisa
+        */
+    }
 }
